3549c86c0e39b966e544964f2f697aa95b1d27e1
stanislav.vdov <svdovenko.startupcraft@gmail.com>
2026-03-08 14:18:43 +0200

    feat: Add JavaScript Prisma configuration and remove comments and `defineConfig` import from TypeScript config.

--- backend/prisma.config.js
+++ backend/prisma.config.js
@@ -1,7 +1,6 @@
-import 'dotenv/config';
-import { defineConfig } from 'prisma/config';
+require('dotenv/config');
 
-export default defineConfig({
+module.exports = {
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
@@ -9,4 +8,4 @@ export default defineConfig({
   datasource: {
     url: process.env.DATABASE_URL,
   },
-});
+};
